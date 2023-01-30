6005d4837a98100f8245cf75b7eb6d833cdda163
manjunath-inboundsys <manjunath.c@inboundsys.com>
2023-01-30 14:55:38 +0530

    fix

--- push.js
+++ push.js
@@ -7,6 +7,14 @@ var vapidKeys = {
 
 push.setVapidDetails('mailto:manjunath.c@inboundsys.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let subscription = {};
+let subscription = {
+  endpoint:
+    'https://fcm.googleapis.com/fcm/send/c3lwlEV3RsU:APA91bGHrcAd5Wm3lWNXMpGfCPdqX6n8W3J25gxW69whm1MMjEC97rMGzfinO2Cre7LqAw10T46zS_W4pgAJjMHfqgZbaz5T-eCW0hRUavRFGL2vY9IC_80CnL1IY6-UdoX3Lcz1IZeP',
+  expirationTime: null,
+  keys: {
+    p256dh: 'BHjkW3AMv8rNnkbjdr12USuz3CS-irRw5C0NdJ1nEmjd2ldAHf8M84OpgpcLzihH5ggvJAltws6BHnYyXm22Oxc',
+    auth: 'A7mnUlf3LdhbTTyvUBnaOQ',
+  },
+};
 
 push.sendNotification(subscription, 'test message');
--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('push', () => {
-  self.registration.sendNotification('test message', {});
+self.addEventListener('push', (e) => {
+  var options = {
+    body: 'This notification was generated from a push',
+    icon: '',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2',
+    },
+    actions: [
+      { action: 'explore', title: 'Explore this new world', icon: 'http://image.ibb.co/frYOFD/tmlogo.png' },
+      { action: 'close', title: 'Close', icon: 'http://image.ibb.co/frYOFD/tmlogo.png' },
+    ],
+  };
+  e.waitUntil(self.registration.showNotification('Hello Push Service', options));
 });
